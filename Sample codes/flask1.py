624815afa9425a746fd720d11f10fc8ea69f8962
vmiovneik <jithendra.vivek@gmail.com>
2017-10-30 17:18:46 +0100

    Sample code using flask

--- Sample codes/flask1.py
+++ Sample codes/flask1.py
@@ -5,6 +5,10 @@ myapp = Flask(__name__)
 
 @myapp.route("/")
 def hi():
-  return "hi"  
-  
+  return "hi \n"  
+
+@myapp.route("/my")
+def m():
+  return "This is function 'm' for the route '/my' \n"  
+
 myapp.run(host='localhost', port=5000, debug=True)    
