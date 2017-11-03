0b5c5cfad8123213211447f98327a7fa7a47993d
vmiovneik <jithendra.vivek@gmail.com>
2017-11-03 19:01:09 +0100

    Update Inferno_db.py

--- Inferno_db.py
+++ Inferno_db.py
@@ -6,4 +6,24 @@ def influx(str):
   client=InfluxDBClient('localhost',8086,'admin','admin',database='vivek3')
   for line in iter(str.readline,''):
 	line=line.rstrip()
-  print line
+	if(line == ''):
+		pass
+	elif():
+		# print line
+		lines=line.split()
+		t=lines[0].split('.')
+		b=lines[1].split('.')
+		json_body=[ 
+              {
+               "measurement": "bitrate",
+               "tags": {
+                   "host": " ",
+                },
+               "fields": {
+                    "value": b[0],
+		    "timestamp": t
+                }
+              }
+             ]
+	        client.write_points(json_body)
+  
