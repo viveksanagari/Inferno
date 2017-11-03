37c9dbb9a33bb1c4efd0346a66d0b855a23a59cb
vmiovneik <jithendra.vivek@gmail.com>
2017-11-03 19:07:43 +0100

    Update Inferno_db.py

--- Inferno_db.py
+++ Inferno_db.py
@@ -3,27 +3,29 @@ from influxdb import InfluxDBClient
 global influx
 
 def influx(str):
-  client=InfluxDBClient('localhost',8086,'admin','admin',database='vivek3')
-  for line in iter(str.readline,''):
-	line=line.rstrip()
-	if(line == ''):
-		pass
-	elif():
-		# print line
-		lines=line.split()
-		t=lines[0].split('.')
-		b=lines[1].split('.')
-		json_body=[ 
-              {
-               "measurement": "bitrate",
-               "tags": {
-                   "host": " ",
-                },
-               "fields": {
-                    "value": b[0],
-		    "timestamp": t
-                }
-              }
-             ]
-	        client.write_points(json_body)
+  	counter=8
+     	linenum=1
+	client=InfluxDBClient('localhost',8086,'admin','admin',database='vivek3')
+  	for line in iter(str.readline,''):
+		line=line.rstrip()
+		if(line == ''):
+			pass
+		elif(linenum > counter):
+			lines=line.split()
+			t=lines[0]
+			b=lines[1].split('.')
+			json_body=[ 
+                      {
+                       "measurement": "bitrate",
+                       "tags": {
+                           "host": " ",
+                        },
+                       "fields": {
+                            "value": b[0],
+		            "timestamp": t
+                        }
+                      }
+                     ]
+	        	client.write_points(json_body)
+		linenum =linenum+1
   
